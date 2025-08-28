18dce6938f5d21a9c19e4839b9dfd2652707325e
menna hamdy <mnh11843@gmail.com>
2025-08-28 12:53:41 +0300

    ....

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,9 @@
 import 'package:flutter/material.dart';
-import 'package:travel_app/features/details/details_screen.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:travel_app/core/dp/firebase_fun.dart';
+import 'package:travel_app/features/auth/cubit/auth_logic.dart';
+import 'package:travel_app/features/auth/models/user_model.dart';
+import 'features/details/details_screen.dart';
 
 void main() {
   runApp(MyApp());
@@ -10,12 +14,31 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'Travel App',
-      theme: ThemeData(primarySwatch: Colors.blue),
+    final dummyPlace = PlaceModel(
+      id: '1',
+      name: 'Andes Mountain',
+      location: 'South America',
+      price: 230,
+      image: 'assets/mountain.png',
+      description:
+          'This vast mountain range is renowned for its remarkable diversity in terms of topography and climate.',
+    );
 
-      home: const DetailScreen(),
+    final dummyUser = AppUser(
+      id: 'user123',
+      name: 'Divad',
+      email: 'divad@example.com',
+      favorites: [],
+    );
+
+    return BlocProvider(
+      create: (_) => DetailsCubit(FirebaseFunctions()),
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        title: 'Travel App',
+        theme: ThemeData(primarySwatch: Colors.blue),
+        home: DetailScreen(place: dummyPlace, user: dummyUser),
+      ),
     );
   }
 }
